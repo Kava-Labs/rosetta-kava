37657ead043624f78da71788b8fc4a8387781ebe
Nick DeLuca <nickdeluca08@gmail.com>
2021-03-30 14:35:19 -0500

    lint

--- kava/client.go
+++ kava/client.go
@@ -1,8 +1,10 @@
 package kava
 
+// Client implements services.Client interface for communcating with the kava chain
 type Client struct {
 }
 
+// NewClient initialized a new Kava Client
 func NewClient() (*Client, error) {
 	return &Client{}, nil
 }
