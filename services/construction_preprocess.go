1977b918f0eb7f471b085d85a68041bd66f4dbce
Nick DeLuca <nickdeluca08@gmail.com>
2021-05-11 19:59:44 -0500

    extract detail out of preprocess method

--- services/construction_preprocess.go
+++ services/construction_preprocess.go
@@ -23,6 +23,7 @@ import (
 	"github.com/kava-labs/rosetta-kava/kava"
 
 	"github.com/coinbase/rosetta-sdk-go/types"
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -38,58 +39,77 @@ func (s *ConstructionAPIService) ConstructionPreprocess(
 		return nil, ErrNoOperations
 	}
 
-	options := make(map[string]interface{})
-
-	var suggestedFeeMultiplier float64
-	if request.SuggestedFeeMultiplier == nil {
-		suggestedFeeMultiplier = defaultSuggestedFeeMultiplier
-	} else {
-		suggestedFeeMultiplier = *request.SuggestedFeeMultiplier
+	encodedMaxFee, err := getMaxFeeAndEncodeOption(request.MaxFee, s.cdc)
+	if err != nil {
+		return nil, err
 	}
-	options["suggested_fee_multiplier"] = suggestedFeeMultiplier
 
-	var memo string
-	if rawMemo, exists := request.Metadata["memo"]; exists {
-		if parsedMemo, ok := rawMemo.(string); ok {
-			memo = parsedMemo
-		}
+	options := map[string]interface{}{
+		"suggested_fee_multiplier": suggestedMultiplerOrDefault(request.SuggestedFeeMultiplier),
+		"memo":                     getMemoFromMetadata(request.Metadata),
 	}
-	options["memo"] = memo
-
-	if request.MaxFee != nil {
-		var maxFee sdk.Coins
-		for _, feeAmount := range request.MaxFee {
-			value, ok := sdk.NewIntFromString(feeAmount.Value)
-			if !ok {
-				return nil, ErrInvalidCurrencyAmount
-			}
-
-			denom, ok := kava.Denoms[feeAmount.Currency.Symbol]
-			if !ok {
-				return nil, ErrUnsupportedCurrency
-			}
-
-			currency, ok := kava.Currencies[denom]
-			if !ok {
-				return nil, ErrUnsupportedCurrency
-			}
-
-			if currency.Symbol != feeAmount.Currency.Symbol ||
-				currency.Decimals != feeAmount.Currency.Decimals {
-				return nil, ErrUnsupportedCurrency
-			}
-
-			maxFee = maxFee.Add(sdk.NewCoin(denom, value))
-		}
-
-		encodedMaxFee, err := s.cdc.MarshalJSON(maxFee)
-		if err != nil {
-			return nil, wrapErr(ErrKava, err)
-		}
-		options["max_fee"] = string(encodedMaxFee)
+	if encodedMaxFee != nil {
+		options["max_fee"] = *encodedMaxFee
 	}
 
 	return &types.ConstructionPreprocessResponse{
 		Options: options,
 	}, nil
+}
+
+func suggestedMultiplerOrDefault(multiplier *float64) float64 {
+	if multiplier == nil {
+		return defaultSuggestedFeeMultiplier
+	}
+
+	return *multiplier
+}
+
+func getMemoFromMetadata(metadata map[string]interface{}) string {
+	if rawMemo, exists := metadata["memo"]; exists {
+		if memo, ok := rawMemo.(string); ok {
+			return memo
+		}
+	}
+
+	return ""
+}
+
+func getMaxFeeAndEncodeOption(amounts []*types.Amount, cdc *codec.Codec) (*string, *types.Error) {
+	if len(amounts) == 0 {
+		return nil, nil
+	}
+
+	var maxFee sdk.Coins
+	for _, feeAmount := range amounts {
+		value, ok := sdk.NewIntFromString(feeAmount.Value)
+		if !ok {
+			return nil, ErrInvalidCurrencyAmount
+		}
+
+		denom, ok := kava.Denoms[feeAmount.Currency.Symbol]
+		if !ok {
+			return nil, ErrUnsupportedCurrency
+		}
+
+		currency, ok := kava.Currencies[denom]
+		if !ok {
+			return nil, ErrUnsupportedCurrency
+		}
+
+		if currency.Symbol != feeAmount.Currency.Symbol ||
+			currency.Decimals != feeAmount.Currency.Decimals {
+			return nil, ErrUnsupportedCurrency
+		}
+
+		maxFee = maxFee.Add(sdk.NewCoin(denom, value))
+	}
+
+	b, err := cdc.MarshalJSON(maxFee)
+	if err != nil {
+		return nil, wrapErr(ErrKava, err)
+	}
+
+	encodedMaxFee := string(b)
+	return &encodedMaxFee, nil
 }
