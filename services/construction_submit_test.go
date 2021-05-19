3ba31954433498f6da54a3ade42dafdaee4d98b6
denalimarsh <denalimarsh@gmail.com>
2021-05-19 17:27:55 +0200

    submit test scaffolding

---
+++ services/construction_submit_test.go
@@ -0,0 +1,83 @@
+// Copyright 2021 Kava Labs, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package services
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/kava-labs/kava/app"
+	"github.com/kava-labs/rosetta-kava/configuration"
+	mocks "github.com/kava-labs/rosetta-kava/mocks/services"
+
+	"github.com/coinbase/rosetta-sdk-go/types"
+	"github.com/stretchr/testify/require"
+)
+
+func validConstructionSubmitRequest(txBytes []byte, blockchain, network string) *types.ConstructionSubmitRequest {
+	networkIdentifier := &types.NetworkIdentifier{
+		Blockchain: blockchain,
+		Network:    network,
+	}
+
+	return &types.ConstructionSubmitRequest{
+		NetworkIdentifier: networkIdentifier,
+		SignedTransaction: hex.EncodeToString(txBytes),
+	}
+}
+
+func TestConstructionSubmit(t *testing.T) {
+	// Set up servicer with mock client
+	cfg := &configuration.Configuration{
+		Mode: configuration.Online,
+	}
+	mockClient := &mocks.Client{}
+	cdc := app.MakeCodec()
+	servicer := NewConstructionAPIService(cfg, mockClient, cdc)
+
+	testCases := []struct {
+		testFixtureFile string
+		expectErr       bool
+		expectedErrCode int32
+	}{
+		{
+			testFixtureFile: "msg-send.json",
+			expectErr:       false,
+		},
+	}
+
+	for _, tc := range testCases {
+		// Load signed transaction from file
+		relPath, err := filepath.Rel(
+			"services",
+			fmt.Sprintf("kava/test-fixtures/signed-msgs/%s", tc.testFixtureFile),
+		)
+		require.NoError(t, err)
+		bz, err := ioutil.ReadFile(relPath)
+		require.NoError(t, err)
+
+		ctx := context.Background()
+		request := validConstructionSubmitRequest(bz, "Kava", "testing")
+		response, rosettaErr := servicer.ConstructionSubmit(ctx, request)
+
+		require.Nil(t, rosettaErr)
+		t.Log(response)
+		t.Fail()
+	}
+}
