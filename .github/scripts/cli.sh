5167c37db7b080459c937b54e2829ca94035cf43
Nick DeLuca <nickdeluca08@gmail.com>
2022-12-05 14:06:21 -0700

    add initial scripts for running check:data in CI (#53)
    * add initial scripts for running check:data in CI
    
    * update ci scripts
    
    * Update setup.sh
    
    * re-test server bootup
    
    * Update setup.sh
    
    * Update ci.yml
    
    * Update setup.sh
    
    * Update ci.yml
    
    * clean up
    
    * change port
    
    Co-authored-by: Xiaying Peng <xiaying.peng@coinbase.com>

---
+++ .github/scripts/cli.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# downloading cli
+curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s
+
+end_idx=($(curl -s --location --request POST 'http://localhost:8000/network/status' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "network_identifier": {
+        "blockchain": "Kava",
+        "network": "kava-testnet"
+    }
+}' | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["current_block_identifier"]["index"])'))
+
+lastest_X_blocks=10
+start_idx=$(($end_idx - $lastest_X_blocks))
+
+echo "start check:data"
+./bin/rosetta-cli --configuration-file rosetta-cli-conf/kava-testnet/config.json check:data --start-block $start_idx --end-block $end_idx
---
+++ .github/scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+MODE=online NETWORK=kava-testnet PORT=8000 KAVA_RPC_URL=http://50.16.212.18:26658 nohup go run . run > /dev/null 2>&1 &
+
+sleep 120
+
+block_tip=($(curl -s --location --request POST 'http://localhost:8000/network/status' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "network_identifier": {
+        "blockchain": "Kava",
+        "network": "kava-testnet"
+    }
+}' | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["current_block_identifier"]["index"])'))
+
+echo "latest block index is", $block_tip
+
