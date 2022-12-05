443de1f7687427dbc8bc74d2a6ec563f5397d4f9
Xiaying Peng <xiaying.peng@coinbase.com>
2022-12-05 12:21:30 -0800

    Update setup.sh

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 
-# MODE=online NETWORK=kava-testnet PORT=8000 KAVA_RPC_URL=http://50.16.212.18:26658 nohup go run . run > /dev/null 2>&1 &
+MODE=online NETWORK=kava-testnet PORT=8000 KAVA_RPC_URL=http://50.16.212.18:26658 nohup go run . run > /dev/null 2>&1 &
 
-ls
-MODE=online NETWORK=kava-testnet PORT=8000 KAVA_RPC_URL=http://50.16.212.18:26658 go run . run
+sleep 120
 
-# sleep 60
+block_tip=($(curl -s --location --request POST 'http://localhost:8000/network/status' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "network_identifier": {
+        "blockchain": "Kava",
+        "network": "kava-testnet"
+    }
+}' | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["current_block_identifier"]["index"])'))
 
-# block_tip=($(curl -s --location --request POST 'http://localhost:8000/network/status' \
-# --header 'Content-Type: application/json' \
-# --data-raw '{
-#     "network_identifier": {
-#         "blockchain": "Kava",
-#         "network": "kava-testnet"
-#     }
-# }' | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["current_block_identifier"]["index"])'))
-
-# echo "latest block index is", $block_tip
+echo "latest block index is", $block_tip
 
