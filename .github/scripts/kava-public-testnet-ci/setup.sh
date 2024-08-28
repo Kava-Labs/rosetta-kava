71396b25d3ed603872b5b462d3f3db7861c25c21
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-08-28 14:25:11 -0400

    Adding kava-public-testnet-ci scripts

---
+++ .github/scripts/kava-public-testnet-ci/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MODE=online NETWORK=kava-testnet PORT=8000 KAVA_RPC_URL=https://rpc.data-testnet.kava.io nohup go run . run > /dev/null 2>&1 &
+
+sleep 5
+
+block_tip=($(curl -s --location --request POST 'http://localhost:8000/network/status' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "network_identifier": {
+        "blockchain": "Kava",
+        "network": "kava-testnet"
+    }
+}' | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["current_block_identifier"]["index"])'))
+
+echo "latest block index is", $block_tip
+
